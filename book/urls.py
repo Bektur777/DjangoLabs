2c588ccc396e056cb369c951010ecff73a8d04dc
Bektur777 <ubei003@mail.ru>
2023-11-04 00:12:58 +0600

    Update project

--- book/urls.py
+++ book/urls.py
@@ -4,7 +4,9 @@ from book.views import *
 urlpatterns = [
     path('', BookListView.as_view()),
     path('book/<int:pk>/', BookDetailView.as_view(), name='detail_book'),
-    path('add-review', ReviewBookCreateView.as_view(), name='review_book'),
-    path('<int:pk>/delete-review', ReviewBookDeleteView.as_view(), name='review_book'),
-    path('reviews', ReviewBookListView.as_view())
+    path('book/add-review', ReviewBookCreateView.as_view(), name='review_book'),
+    path('book/<int:pk>/update-review', ReviewBookUpdateView.as_view(), name='review_book'),
+    path('book/<int:pk>/delete-review', ReviewBookDeleteView.as_view(), name='review_book'),
+    path('book/reviews', ReviewBookListView.as_view()),
+    path('book/search/', Search.as_view(), name='search'),
 ]
--- book/views.py
+++ book/views.py
@@ -14,6 +14,23 @@ class BookListView(ListView):
     template_name = 'books.html'
 
 
+class Search(ListView):
+    paginate_by = 3
+
+    model = Book
+    queryset = Book.objects.all()
+    context_object_name = 'books'
+    template_name = 'books.html'
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains=self.request.GET.get('search'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['search'] = f'search={self.request.GET.get("search")}&'
+        return context
+
+
 class BookDetailView(DetailView):
     model = Book
     queryset = Book.objects.all()
@@ -33,6 +50,13 @@ class ReviewBookCreateView(CreateView):
     form_class = ReviewBookForm
     success_url = '/'
     template_name = 'book_detail.html'
+
+
+class ReviewBookUpdateView(UpdateView):
+    model = ReviewBook
+    form_class = ReviewBookForm
+    success_url = '/'
+    template_name = 'book_update.html'
 
 
 class ReviewBookDeleteView(DeleteView):
